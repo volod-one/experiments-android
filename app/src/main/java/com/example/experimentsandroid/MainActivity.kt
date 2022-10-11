8b73a1514e9f2a55dea336a6c07ab854e1be35f3
Volodymyr Balashov <xaosssss@gmail.com>
2022-10-11 15:11:08 +0900

    implemented viewBinding

--- app/src/main/java/com/example/experimentsandroid/MainActivity.kt
+++ app/src/main/java/com/example/experimentsandroid/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.experimentsandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.experimentsandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
